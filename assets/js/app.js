44e2574a9269cff412a7de685839c7495e4c79b7
rabin999 <rabin.bhandari999@gmail.com>
2018-05-19 00:08:01 +0545

    utitlity feature like event, addClass, removeClass, on, eventListener added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,139 +1,248 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+/**
+ *	Copyright
+ *	Rabin Bhandari
+ */
+
+// ------------------------------------------------
+// Utility
+(function () {
+    if (!window.rb) {
+        window.rb = function (selector) {
+
+            var element = null;
+
+            // Prepare Node || Nodes
+            if (typeof selector === 'string' && selector.length) {
+                element = document.querySelectorAll(selector);
+            } else if ((typeof selector === 'undefined' ? 'undefined' : _typeof(selector)) === 'object') {
+                element = selector;
+            } else {
+                error('Selector not defined');
+            }
+
+            // Check Node found or not 
+            if (element instanceof HTMLElement || element instanceof Node || element instanceof NodeList) {
+                if (element instanceof NodeList) {
+                    if (!element.length) return element = false;
+                }
+            }
+
+            function error(message) {
+                if (message) {
+                    if (window.Error) {
+                        throw new Error(message);
+                    } else if (window.console) {
+                        console.error(message);
+                    }
+                }
+            }
+
+            /*
+            * ---------------
+            * Events
+            * ---------------
+            */
+            function on(event, callback) {
+                __manageEvent(event, 'addEventListener', callback);
+            }
+
+            function off(event, callback) {
+                __manageEvent(event, 'removeEventListener', callback);
+            }
+
+            /*
+            * ---------------
+            * Class
+            * ---------------
+            */
+            function addClass(classes) {
+                __manageClass(classes, 'add');
+            }
+
+            function removeClass(classes) {
+                __manageClass(classes, 'remove');
+            }
+
+            // Private Functions
+
+
+            // Check where given event is exist or not
+            function __hasEvent(event, element) {
+                return typeof element['on' + event] !== "undefined";
+            }
+
+            function __manageEvent(event, operation, callback) {
+
+                if (!(window[operation] || callback)) {
+                    error('Event || Event Function reference not found');
+                }
+
+                if (window[operation]) {
+                    if (element === element.window || element.NodeType === 9) {
+                        if (__hasEvent(event, element)) {
+                            element[operation](event, callback);
+                        }
+                    } else {
+                        element.forEach(function (el) {
+                            if (__hasEvent(event, el)) {
+                                el[operation](event, callback);
+                            }
+                        });
+                    }
+                }
+            }
+
+            function __manageClass(classes, operation) {
+
+                if (!classes) {
+                    error('Class arguments not founds !');
+                }
+
+                var classLists = classes.trim().split(' ');
+
+                if (element === element.window || element.NodeType === 9) {
+                    classLists.forEach(function (cl) {
+                        element.classList[operation](cl);
+                    });
+                } else {
+                    element.forEach(function (el) {
+                        classLists.forEach(function (cl, index) {
+                            el.classList[operation](cl);
+                        });
+                    });
+                }
+            }
+
+            // Attributes
+            function attr(name, value) {
+                if (element === element.window || element.NodeType === 9) {
+                    element.setAttribute(name, value);
+                } else {
+                    element.forEach(function (el) {
+                        el.setAttribute(name, value);
+                    });
+                }
+            }
+
+            function removeAttr(name) {
+                if (element === element.window || element.NodeType === 9) {
+                    if (element.hasAttribute(name)) element.removeAttribute(name);
+                } else {
+                    element.forEach(function (el) {
+                        if (el.hasAttribute(name)) el.removeAttribute(name);
+                    });
+                }
+            }
+
+            if (element) return {
+                on: on,
+                off: off,
+                addClass: addClass,
+                removeClass: removeClass,
+                attr: attr,
+                removeAttr: removeAttr
+            };
+        };
+    }
+})();
+
+// Utility End
+// -------------------------------------------------
+
+
 // Load Event
-window.addEventListener("load", function () {
-	ready();
+rb(window).on("load", function () {
+    ready();
 });
 
 // Resize Event
-window.addEventListener('resize', setContainerHeight);
+rb(window).on('resize', setContainerHeight);
 
 function ready() {
-	setContainerHeight();
+    setContainerHeight();
 };
 
 // Set Container Width
 function setContainerHeight() {
-	var header = document.querySelector('header').offsetHeight,
-	    footer = document.querySelector('footer').offsetHeight;
-	document.querySelector('main.container').setAttribute("style", 'height:' + (window.innerHeight - header - footer) + 'px; min-height:' + (window.innerHeight - header - footer) + 'px;');
+    var header = document.querySelector('header').offsetHeight,
+        footer = document.querySelector('footer').offsetHeight,
+        height = window.innerHeight - header - footer - 40;
+    var rule = 'height:' + height + 'px; min-height:' + height + 'px;';
+
+    rb('main.container').attr("style", rule);
 }
 
-// Mouse Scroller Event
-document.querySelector('div.mouse-scroller').addEventListener("mouseover", function (e) {
-	this.classList.remove('bounce');
-});
-
-document.querySelector('div.mouse-scroller').addEventListener("mouseout", function (e) {
-	this.classList.add('bounce');
-});
-
-document.querySelector('div.mouse-scroller').addEventListener("click", function (e) {
-	nextPage();
-});
-
-if (window.addEventListener) {
-	window.addEventListener('mousewheel', function (e) {
-		if (e.deltaY < 0) {
-			prevPage();
-		}
-		if (e.deltaY > 0) {
-			nextPage();
-		}
-	});
-} else {
-	window.attachEvent('onmousewheel', function (e) {
-		if (e.deltaY < 0) {
-			prevPage();
-		}
-		if (e.deltaY > 0) {
-			nextPage();
-		}
-	});
-}
-
-// Add Page Switch Transition
-function switchPage(from, to) {
-
-	from.classList.remove('left');
-	from.classList.add('right');
-
-	// Add tranformation start Point
-	to.style.transform = 'translateX(110%)';
-
-	var leftPage = setTimeout(function () {
-		to.style.transform = 'none';
-		clearTimeout(leftPage);
-
-		document.querySelectorAll('.pages').forEach(function (element) {
-			element.classList.remove('active');
-			element.classList.remove('right');
-			document.querySelector('.progress-bar').style.width = 'auto';
-		});
-
-		to.classList.add('active');
-		to.classList.add('left');
-	}, 500);
-}
-
-// Add active class to page switcher
-function activePageSwitcher(ref) {
-	if (ref) {
-
-		// Hide all pages div and add active to last
-		document.querySelectorAll('.switch-page').forEach(function (element) {
-			element.classList.remove('active');
-		});
-		ref.classList.add('active');
-	}
-}
-
-function nextPage() {
-	var activePage = document.querySelector('.pages.active');
-	var nextPage = activePage.nextElementSibling;
-	var switchPage = document.querySelector('.switch-page[data-page=' + nextPage.getAttribute('data-page') + ']');
-	if (nextPage) {
-		switchPage(activePage, nextPage);
-	}
-}
-
-function prevPage() {
-	var activePage = document.querySelector('.pages.active');
-	var prevPage = activePage.previousElementSibling;
-	var switchPage = document.querySelector('.switch-page[data-page=' + prevPage.getAttribute('data-page') + ']');
-	if (prevPage) {
-		switchPage(activePage, prevPage);
-	}
-}
-
-// Mouse Scroller Event : END
-
-// Page Switcher Event
-document.querySelectorAll('.switch-page').forEach(function (element) {
-	element.addEventListener('click', function (e) {
-		e.preventDefault();
-		var page = this.getAttribute('data-page');
-
-		// Add Active class to triggered page
-		initProgress();
-
-		// Switch Page
-		switchPage(document.querySelector('.pages.active'), document.querySelector('.pages[data-page=' + page + ']'));
-		activePageSwitcher(this);
-	});
+// Scroll Event For Percentage
+document.querySelector('main.container').addEventListener("scroll", function (e) {
+    var activePageHeight = parseInt(this.scrollHeight - this.clientHeight);
+    var totalProcess = Math.round(this.scrollTop / activePageHeight * 100);
+    rb('.progress-bar').addClass('page-process');
+    document.querySelector('.progress-bar').style.width = totalProcess + "%";
 });
 
 // Progress Bar
 function initProgress(progress) {
-	var percent = 5;
-	document.querySelector('.progress-bar').style.width = 'auto';
-	if (progress === undefined) {
-		var p = setInterval(function () {
-			document.querySelector('.progress-bar').style.width = percent + '%';
-			percent += 5;
-			if (percent > 100) {
-				clearInterval(p);
-			}
-		}, 0);
-	}
+    var percent = 5;
+    document.querySelector('.progress-bar').style.width = 'auto';
+    if (progress === undefined) {
+        var p = setInterval(function () {
+            document.querySelector('.progress-bar').style.width = percent + '%';
+            percent += 5;
+            if (percent > 100) {
+                clearInterval(p);
+            }
+        }, 0);
+    }
 }
+
+// Page Animation on double click
+
+// document.addEventListener("dblclick", (e) => {
+//     let page = document.querySelector('.intro-text');
+//     let size = parseInt((innerHeight / 100) * 80);
+//     page.style.height = size+"px";
+//     page.style.width = size+"px";
+//     page.classList.remove('open-page');
+//     page.classList.add('close-page');
+// });
+
+
+// Form Elements
+rb('.input > input').on('focus', function (e) {
+    if (this.nodeName === 'INPUT') {
+        if (this.previousElementSibling.nodeName === 'LABEL') {
+            this.previousElementSibling.classList.add('top');
+        }
+    }
+});
+
+rb('.input > input').on('blur', function (e) {
+    if (this.nodeName === 'INPUT') {
+        if (this.previousElementSibling.nodeName === 'LABEL') {
+            if (this.value.length) {
+                this.previousElementSibling.classList.add('top');
+            } else {
+                this.previousElementSibling.classList.remove('top');
+            }
+        }
+    }
+});
+
+// ChatBox Event
+rb('.chatBox .close').on('click', function (e) {
+    document.querySelector('.chatBox').style.transform = 'scale(0)';
+    setTimeout(function () {
+        rb('.chatBox').addClass('hidden');
+    }, 500);
+    document.querySelector('.startChat').classList.remove('hidden');
+});
+
+rb('.startChat').on('click', function (e) {
+    rb('.chatBox').removeClass('hidden');
+    document.querySelector('.chatBox').style.transform = 'scale(1)';
+    this.classList.add('hidden');
+});
