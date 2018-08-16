0042a78cc38d70331f42be01f39b211d478d983b
rabin999 <rabin.bhandari999@gmail.com>
2018-08-16 14:17:28 +0545

    static files updated

--- single-blog.php
+++ single-blog.php
@@ -313,7 +313,186 @@
         <!-- Blogs end -->
 
         <!-- Right Sidebar -->
-        <?php include('./sidebar.php'); ?>
+        <aside class="summary">
+            <!-- Most Reviewed -->
+            <div class="most-viewed">
+                <h2 class="header">Most Viewed</h2>
+                <div class="blogs">
+                    <div class="blog">
+                        <a href="" class="blog-image">
+                            <img src="./assets/images/blog/first.jpg" alt="">
+                        </a>
+                        <article>
+                            <a class="title" href="">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quia.
+                            </a>
+
+                            <div class="row margin-tb-20">
+                                <div class="col">
+                                    <a class="tag" href="" title="Blog Tag">Engeneering</a>
+                                </div>
+                                <div class="col">
+                                    <div class="view-comment">
+                                <span class="view" title="total view count">
+                                    <svg aria-hidden="true" data-prefix="far" data-icon="eye" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                            <path fill="currentColor" d="M569.354 231.631C512.97 135.949 407.81 72 288 72 168.14 72 63.004 135.994 6.646 231.631a47.999 47.999 0 0 0 0 48.739C63.031 376.051 168.19 440 288 440c119.86 0 224.996-63.994 281.354-159.631a47.997 47.997 0 0 0 0-48.738zM288 392c-102.556 0-192.091-54.701-240-136 44.157-74.933 123.677-127.27 216.162-135.007C273.958 131.078 280 144.83 280 160c0 30.928-25.072 56-56 56s-56-25.072-56-56l.001-.042C157.794 179.043 152 200.844 152 224c0 75.111 60.889 136 136 136s136-60.889 136-136c0-31.031-10.4-59.629-27.895-82.515C451.704 164.638 498.009 205.106 528 256c-47.908 81.299-137.444 136-240 136z"></path></svg>28
+                                </span>
+                                        <span class="comment" title="total comment count">
+                                    <svg aria-hidden="true" data-prefix="fas" data-icon="comments" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                                    <path fill="currentColor" d="M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z"></path></svg> 26
+                                </span>
+                                    </div>
+                                </div>
+                                <div class="col published-date">
+                                    <a href="" title="Published Date">
+                                        <div class="date"><svg aria-hidden="true" data-prefix="far" data-icon="calendar-alt" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                <path fill="currentColor" d="M148 288h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm108-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 96v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96-260v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48zm-48 346V160H48v298c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"></path></svg>
+                                            February 16, 2018</div>
+                                    </a>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div class="blog">
+                        <a href="" class="blog-image">
+                            <img src="./assets/images/blog/first.jpg" alt="">
+                        </a>
+                        <article>
+                            <a class="title" href="">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quia.
+                            </a>
+
+                            <div class="row margin-tb-20">
+                                <div class="col">
+                                    <a class="tag" href="" title="Blog Tag">Engeneering</a>
+                                </div>
+                                <div class="col">
+                                    <div class="view-comment">
+										<span class="view" title="total view count">
+											<svg aria-hidden="true" data-prefix="far" data-icon="eye" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                            <path fill="currentColor" d="M569.354 231.631C512.97 135.949 407.81 72 288 72 168.14 72 63.004 135.994 6.646 231.631a47.999 47.999 0 0 0 0 48.739C63.031 376.051 168.19 440 288 440c119.86 0 224.996-63.994 281.354-159.631a47.997 47.997 0 0 0 0-48.738zM288 392c-102.556 0-192.091-54.701-240-136 44.157-74.933 123.677-127.27 216.162-135.007C273.958 131.078 280 144.83 280 160c0 30.928-25.072 56-56 56s-56-25.072-56-56l.001-.042C157.794 179.043 152 200.844 152 224c0 75.111 60.889 136 136 136s136-60.889 136-136c0-31.031-10.4-59.629-27.895-82.515C451.704 164.638 498.009 205.106 528 256c-47.908 81.299-137.444 136-240 136z"></path></svg>28
+										</span>
+                                        <span class="comment" title="total comment count">
+											<svg aria-hidden="true" data-prefix="fas" data-icon="comments" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                                    <path fill="currentColor" d="M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z"></path></svg> 26
+										</span>
+                                    </div>
+                                </div>
+                                <div class="col published-date">
+                                    <a href="" title="Published Date">
+                                        <div class="date"><svg aria-hidden="true" data-prefix="far" data-icon="calendar-alt" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                <path fill="currentColor" d="M148 288h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm108-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 96v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96-260v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48zm-48 346V160H48v298c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"></path></svg>
+                                            February 16, 2018</div>
+                                    </a>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div class="blog">
+                        <a href="" class="blog-image">
+                            <img src="./assets/images/blog/first.jpg" alt="">
+                        </a>
+                        <article>
+                            <a class="title" href="">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quia.
+                            </a>
+
+                            <div class="row margin-tb-20">
+                                <div class="col">
+                                    <a class="tag" href="" title="Blog Tag">Engeneering</a>
+                                </div>
+                                <div class="col">
+                                    <div class="view-comment">
+										<span class="view" title="total view count">
+											<svg aria-hidden="true" data-prefix="far" data-icon="eye" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                            <path fill="currentColor" d="M569.354 231.631C512.97 135.949 407.81 72 288 72 168.14 72 63.004 135.994 6.646 231.631a47.999 47.999 0 0 0 0 48.739C63.031 376.051 168.19 440 288 440c119.86 0 224.996-63.994 281.354-159.631a47.997 47.997 0 0 0 0-48.738zM288 392c-102.556 0-192.091-54.701-240-136 44.157-74.933 123.677-127.27 216.162-135.007C273.958 131.078 280 144.83 280 160c0 30.928-25.072 56-56 56s-56-25.072-56-56l.001-.042C157.794 179.043 152 200.844 152 224c0 75.111 60.889 136 136 136s136-60.889 136-136c0-31.031-10.4-59.629-27.895-82.515C451.704 164.638 498.009 205.106 528 256c-47.908 81.299-137.444 136-240 136z"></path></svg>28
+										</span>
+                                        <span class="comment" title="total comment count">
+											<svg aria-hidden="true" data-prefix="fas" data-icon="comments" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                                    <path fill="currentColor" d="M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z"></path></svg> 26
+										</span>
+                                    </div>
+                                </div>
+                                <div class="col published-date">
+                                    <a href="" title="Published Date">
+                                        <div class="date"><svg aria-hidden="true" data-prefix="far" data-icon="calendar-alt" class="has-margin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                <path fill="currentColor" d="M148 288h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm108-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 96v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96-260v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48zm-48 346V160H48v298c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"></path></svg>
+                                            February 16, 2018</div>
+                                    </a>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Category -->
+            <div class="categories">
+                <h2 class="header">Category</h2>
+                <ul>
+                    <li class="category"><a href="">Data Structure</a></li>
+                    <li class="category"><a href="">Algorithm</a></li>
+                    <li class="category"><a href="">Computer Science</a></li>
+                    <li class="category"><a href="">JavaScript</a></li>
+                    <li class="category"><a href="">Java</a></li>
+                    <li class="category"><a href="">PHP</a></li>
+                    <li class="category"><a href="">Python</a></li>
+                    <li class="category"><a href="">Dreams</a></li>
+                    <li class="category"><a href="">Entrepreneurship</a></li>
+                </ul>
+            </div>
+
+
+            <!-- Tags -->
+            <div class="tags">
+                <h2 class="header">Tags</h2>
+                <ul>
+                    <li class="tag"><a href="">Science</a></li>
+                    <li class="tag"><a href="">Computer</a></li>
+                    <li class="tag"><a href="">JavaScript</a></li>
+                    <li class="tag"><a href="">Java</a></li>
+                    <li class="tag"><a href="">PHP</a></li>
+                    <li class="tag"><a href="">Python</a></li>
+                    <li class="tag"><a href="">Dreams</a></li>
+                    <li class="tag"><a href="">Entrepreneurship</a></li>
+                </ul>
+            </div>
+
+            <!-- Archive -->
+            <div class="archives">
+                <h2 class="header">Archives</h2>
+                <ul>
+                    <li class="tag"><a href="">Science <small>29 posts</small></a></li>
+                    <li class="tag"><a href="">Science <small>29 posts</small></a></li>
+                    <li class="tag"><a href="">Science <small>29 posts</small></a></li>
+                    <li class="tag"><a href="">Science <small>29 posts</small></a></li>
+                </ul>
+            </div>
+
+            <!-- Get Notified -->
+            <div class="get-notified">
+                <h2>
+                    Get Notified
+                </h2>
+                <p>
+                    Be the first to know when there are new reasearch related to Development, Design, Web
+                    Performance, Tips and Tricks and inspirational articles on Blog.
+                </p>
+                <div class="subscription-form">
+                    <div class="rb-form">
+                        <input type="text" placeholder="Name">
+                        <input type="email" placeholder="email">
+                        <div class="subscribe-btn">
+                            <button>Subscribe</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Get Notified:END -->
+        </aside>
 
     </main>
     <!-- Container: END -->
